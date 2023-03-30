2a3ce07c2fd9d7ece6b133e19483356740f3e809
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-03-30 16:39:26 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+add = (firstNumber, secondNumber) => firstNumber + secondNumber
+subtract = (firstNumber, secondNumber) => firstNumber - secondNumber
+multiply = (firstNumber, secondNumber) => firstNumber * secondNumber
+divide = (firstNumber, secondNumber) => firstNumber / secondNumber
+function increment(n)  {
+    n++
+    return n 
+}
+function decrement(n)  {
+    n--
+    return n 
+}
+
+makeInt = (string) => parseInt(string, 10)
+preserveDecimal = (string) => parseFloat(string)
